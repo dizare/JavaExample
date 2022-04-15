de5206a6bf2821ca04d7eaed18a5212421847c36
dizare <bets4pro@mail.ru>
2022-04-15 22:45:45 +0500

    Scanner + print

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public Main() {
     }
 
     public static void main(String[] args) {
+        Scanner hew = new Scanner(System.in);
+        System.out.print("Введите X:");
+        float X = hew.nextFloat();
+        System.out.println("Вы ввели это число:" + X);
+
     }
 }
