b226772a8a7ee3c50ec8e7643f820c3a99b3f59d
dizare <bets4pro@mail.ru>
2022-04-15 22:59:54 +0500

    Сalculating the area

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,9 +8,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner hew = new Scanner(System.in);
-        System.out.print("Введите X:");
-        float X = hew.nextFloat();
-        System.out.println("Вы ввели это число:" + X);
+        System.out.print("Введите сторону a: ");
+        float a = hew.nextFloat();
+        System.out.print("Введите сторону b: ");
+        float b = hew.nextFloat();
+        System.out.print("Введите угол между ними: ");
+        float alph = hew.nextFloat();
+        double S = 0.5 * a * b * Math.sin(alph);
+        System.out.println("Площадь треугольника :" + S);
+
 
     }
 }
